57241c46fece241339ec367fcdc2fe8578b2f7ea
Sean Molenaar <smillerdev@me.com>
2019-05-15 10:36:10 +0200

    pcov 1.0.3 (new formula)

---
+++ Formula/pcov.rb
@@ -0,0 +1,43 @@
+class Pcov < Formula
+  desc "CodeCoverage compatible driver for PHP7"
+  homepage "https://github.com/krakjoe/pcov"
+  url "https://github.com/krakjoe/pcov/archive/v1.0.3.tar.gz"
+  sha256 "094a5c172c3ca8e35931ca6ebb6acf853432d522ecdbd3a2f031f5cc69f20510"
+
+  depends_on "php"
+
+  def module_path
+    extension_dir = Utils.popen_read("#{Formula["php"].opt_bin/"php-config"} --extension-dir").chomp
+    php_basename = File.basename(extension_dir)
+    "php/#{php_basename}"
+  end
+
+  def install
+    system Formula["php"].bin/"phpize"
+    configure_args = %W[
+      --with-php-config=#{Formula["php"].opt_bin/"php-config"}
+      --enable-pcov
+    ]
+    system "./configure", *configure_args
+    system "make"
+    (lib/module_path).install "modules/pcov.so"
+  end
+
+  def post_install
+    ext_config_path = etc/"php"/Formula["php"].php_version/"conf.d"/"ext-pcov.ini"
+    if ext_config_path.exist?
+      inreplace ext_config_path,
+        /extension=.*$/, "extension=\"#{opt_lib/module_path}/pcov.so\""
+    else
+      ext_config_path.write <<~EOS
+        [pcov]
+        extension=\"#{opt_lib/module_path}/pcov.so\"
+      EOS
+    end
+  end
+
+  test do
+    assert_match "pcov", shell_output("#{Formula["php"].opt_bin}/php -m").downcase,
+      "failed to find extension in php -m output"
+  end
+end
