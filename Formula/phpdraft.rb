b31ea6f4d8ab023daec4fe9538f650a8b76fc5e4
Sean Molenaar <smillerdev@me.com>
2019-05-27 15:15:15 +0200

    phpdraft 1.14.1.2 (fix test)

--- Formula/phpdraft.rb
+++ Formula/phpdraft.rb
@@ -1,110 +1,29 @@
-class PhpunitAT7 < Formula
-  desc "Programmer-oriented testing framework for PHP"
-  homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-7.5.11.phar"
-  sha256 "17550f7864c66bf4b703d13378da678ee8e2eef31b81f37f24da01de0634c1a6"
+class Phpdraft < Formula
+  desc "Parser for API Blueprint files in PHP"
+  homepage "https://github.com/SMillerDev/phpdraft"
+  url "https://github.com/SMillerDev/phpdraft/releases/download/1.14.1.2/phpdraft-1.14.1.2.phar"
+  sha256 "6b902b1f95fe3c2d3326be3d292308c7314e1176d21c702092f250245ec256b3"
 
   bottle :unneeded
 
+  depends_on "drafter"
   depends_on "php" => :test
 
   def install
-    bin.install "phpunit-#{version}.phar" => "phpunit@7"
+    bin.install "phpdraft-#{version}.phar" => "phpdraft"
   end
 
   test do
-    (testpath/"src/autoload.php").write <<~EOS
-      <?php
-      spl_autoload_register(
-          function($class) {
-              static $classes = null;
-              if ($classes === null) {
-                  $classes = array(
-                      'email' => '/Email.php'
-                  );
-              }
-              $cn = strtolower($class);
-              if (isset($classes[$cn])) {
-                  require __DIR__ . $classes[$cn];
-              }
-          },
-          true,
-          false
-      );
-    EOS
+    (testpath/"index.apib").write <<~EOS
+      FORMAT: 1A
+      HOST: https://alpha-api.app.net
+      EXTRA_HOST: https://test.alpha-api.app.net
 
-    (testpath/"src/Email.php").write <<~EOS
-      <?php
-        declare(strict_types=1);
-
-        final class Email
-        {
-            private $email;
-
-            private function __construct(string $email)
-            {
-                $this->ensureIsValidEmail($email);
-
-                $this->email = $email;
-            }
-
-            public static function fromString(string $email): self
-            {
-                return new self($email);
-            }
-
-            public function __toString(): string
-            {
-                return $this->email;
-            }
-
-            private function ensureIsValidEmail(string $email): void
-            {
-                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    throw new InvalidArgumentException(
-                        sprintf(
-                            '"%s" is not a valid email address',
-                            $email
-                        )
-                    );
-                }
-            }
-        }
-    EOS
-
-    (testpath/"tests/EmailTest.php").write <<~EOS
-      <?php
-      declare(strict_types=1);
-
-      use PHPUnit\\Framework\\TestCase;
-
-      final class EmailTest extends TestCase
-      {
-          public function testCanBeCreatedFromValidEmailAddress(): void
-          {
-              $this->assertInstanceOf(
-                  Email::class,
-                  Email::fromString('user@example.com')
-              );
-          }
-
-          public function testCannotBeCreatedFromInvalidEmailAddress(): void
-          {
-              $this->expectException(InvalidArgumentException::class);
-
-              Email::fromString('invalid');
-          }
-
-          public function testCanBeUsedAsString(): void
-          {
-              $this->assertEquals(
-                  'user@example.com',
-                  Email::fromString('user@example.com')
-              );
-          }
-      }
+      # Real World API
+      This API Blueprint demonstrates a real world example documenting a portion of
+      [App.net API](http://developers.app.net).
 
     EOS
-    assert_match /^OK \(3 tests, 3 assertions\)$/, shell_output("#{bin}/phpunit@7 --bootstrap src/autoload.php tests/EmailTest")
+    assert_match /<title>Real World API<\/title>/, shell_output("#{bin}/phpdraft --file index.apib")
   end
 end
