5c6c015ad4b1324dc79da85ad91eed54ce251c9b
Sean Molenaar <smillerdev@me.com>
2020-05-25 18:44:37 +0200

    add phpcpd

---
+++ Formula/phpcpd.rb
@@ -0,0 +1,14 @@
+class Phpcpd < Formula
+  desc "Programmer-oriented testing framework for PHP"
+  homepage "https://phpunit.de"
+  url "https://phar.phpunit.de/phpcpd-5.0.2.phar"
+  sha256 "12e94af9928be2f33fd394d2cc5d96b3925de24d614381d06b3096cc9eeabbcb"
+      
+  bottle :unneeded
+        
+  depends_on "php" => :test
+            
+  def install
+    bin.install "phpcpd-#{version}.phar" => "phpcpd"
+  end
+end
