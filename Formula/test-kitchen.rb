6fe61f26f37ff64035d0a2baa739f5d2e90c7ce0
Sean Molenaar <smillerdev@me.com>
2020-08-03 08:48:12 +0200

    test-kitchen 2.5.4

--- Formula/test-kitchen.rb
+++ Formula/test-kitchen.rb
@@ -1,8 +1,8 @@
 class TestKitchen < Formula
   desc "Tool for developing and testing infrastructure code"
   homepage "https://kitchen.ci"
-  url "https://github.com/test-kitchen/test-kitchen/archive/v2.5.3.tar.gz"
-  sha256 "a6642903698572d7c4a7e606d17cf6f5a2de6d5bb4f19879b29f2a30b9066416"
+  url "https://github.com/test-kitchen/test-kitchen/archive/v2.5.4.tar.gz"
+  sha256 "18c325af38c0b022fe1d8296d0353e45bf52bc03bbf68aac9085e0f05baf0d55"
   head "https://github.com/test-kitchen/test-kitchen.git"
 
   depends_on "ruby" if MacOS.version <= :sierra
@@ -19,8 +19,8 @@ class TestKitchen < Formula
     system "gem", "install", "kitchen-salt", "--norc", "--no-document"
     system "gem", "install", "kitchen-vagrant", "--norc", "--no-document"
 
-    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
-    (bin/"kitchen").write_env_script(libexec/"bin/kitchen", :GEM_PATH => libexec)
+    bin.env_script_all_files(libexec/"bin", GEM_HOME => ENV["GEM_HOME"])
+    (bin/"kitchen").write_env_script(libexec/"bin/kitchen", GEM_PATH => libexec)
   end
 
   test do
