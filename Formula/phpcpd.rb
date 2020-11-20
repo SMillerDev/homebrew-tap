f925feec42c5af9c72d7c47ecb0912f4d28df882
Sean Molenaar <smillerdev@me.com>
2020-11-20 11:00:26 +0100

    phpcpd: fix style

--- Formula/phpcpd.rb
+++ Formula/phpcpd.rb
@@ -3,11 +3,11 @@ class Phpcpd < Formula
   homepage "https://phpunit.de"
   url "https://phar.phpunit.de/phpcpd-5.0.2.phar"
   sha256 "12e94af9928be2f33fd394d2cc5d96b3925de24d614381d06b3096cc9eeabbcb"
-      
+
   bottle :unneeded
-        
+
   depends_on "php" => :test
-            
+
   def install
     bin.install "phpcpd-#{version}.phar" => "phpcpd"
   end
