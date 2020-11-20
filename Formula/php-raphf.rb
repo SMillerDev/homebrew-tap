4ce2b38a27bcd32660440a8165d7bc3976a3e9fe
Sean Molenaar <smillerdev@me.com>
2020-11-20 11:01:41 +0100

    php-raphf: fix style

--- Formula/php-raphf.rb
+++ Formula/php-raphf.rb
@@ -10,7 +10,7 @@ class PhpRaphf < Formula
   depends_on "php"
 
   def module_path
-    extension_dir = Utils.popen_read("#{Formula["php"].opt_bin/"php-config"} --extension-dir").chomp
+    extension_dir = Utils.safe_popen_read("#{Formula["php"].opt_bin/"php-config"} --extension-dir").chomp
     php_basename = File.basename(extension_dir)
     "php/#{php_basename}"
   end
