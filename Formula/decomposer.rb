a33aa0980ff8896ce9c92d1d755c947c684490aa
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-01-18 16:20:53 +0000

    decomposer: add 1.0.0 bottle.

--- Formula/decomposer.rb
+++ Formula/decomposer.rb
@@ -5,6 +5,13 @@ class Decomposer < Formula
   sha256 "b52f3b23645ca0872cacf80e564ecc987188a623136762d6fc161334c0c63ad8"
   head "https://github.com/M2mobi/decomposer.git"
 
+  bottle do
+    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/decomposer-1.0.0"
+    cellar :any_skip_relocation
+    sha256 "1504fd2a8baa5595b080953ddb821b53f9b144039bd7ff38604e61cf46ca70d3" => :catalina
+    sha256 "f3ccc4198912dc0e5f6d956180456b6963fd01151300c97b29d5d2e4a771079f" => :x86_64_linux
+  end
+
   depends_on "scdoc" => :build
   depends_on "coreutils"
   depends_on "jq"
