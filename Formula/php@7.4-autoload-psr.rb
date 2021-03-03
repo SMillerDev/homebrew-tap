14b0950a130bc46b4f8204f757122b9e3b771123
Sean Molenaar <smillerdev@me.com>
2021-03-03 10:45:05 +0100

    php-autoload-psr: split for php 7.4

--- Formula/php@7.4-autoload-psr.rb
+++ Formula/php@7.4-autoload-psr.rb
@@ -1,30 +1,28 @@
-class PhpAutoloadPsr < Formula
+class PhpAT74AutoloadPsr < Formula
   desc "Autoload-PSR Extension for PHP"
   homepage "https://github.com/pprkut/autoload-psr"
   url "https://github.com/pprkut/autoload-psr/archive/0.1.2.tar.gz"
   sha256 "edee4c0fb8d66238509e080697a868ad454444242ffb269cb52c6cceabd04c2c"
   head "https://github.com/pprkut/autoload-psr.git"
 
-  disable! date: "2021-03-01", because: "support pending"
-
   depends_on "autoconf" => :build
-  depends_on "php"
+  depends_on "php@7.4"
 
   def module_path
-    extension_dir = Utils.safe_popen_read("#{Formula["php"].opt_bin/"php-config"} --extension-dir").chomp
+    extension_dir = Utils.safe_popen_read("#{Formula["php@7.4"].opt_bin/"php-config"} --extension-dir").chomp
     php_basename = File.basename(extension_dir)
     "php/#{php_basename}"
   end
 
   def install
-    system Formula["php"].bin/"phpize"
-    system "./configure", "--with-php-config=#{Formula["php"].opt_bin/"php-config"}"
+    system Formula["php@7.4"].bin/"phpize"
+    system "./configure", "--with-php-config=#{Formula["php@7.4"].opt_bin/"php-config"}"
     system "make"
     (lib/module_path).install "modules/autoload_psr.so"
   end
 
   def post_install
-    ext_config_path = etc/"php"/Formula["php"].version.major_minor/"conf.d"/"ext-autoload_psr.ini"
+    ext_config_path = etc/"php"/Formula["php@7.4"].version.major_minor/"conf.d"/"ext-autoload_psr.ini"
     if ext_config_path.exist?
       inreplace ext_config_path,
         /extension=.*$/, "extension=\"#{opt_lib/module_path}/autoload_psr.so\""
@@ -37,7 +35,7 @@ class PhpAutoloadPsr < Formula
   end
 
   test do
-    assert_match "autoload_psr", shell_output("#{Formula["php"].opt_bin}/php -m").downcase,
+    assert_match "autoload_psr", shell_output("#{Formula["php@7.4"].opt_bin}/php -m").downcase,
       "failed to find extension in php -m output"
   end
 end
