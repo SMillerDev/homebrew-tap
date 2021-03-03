27a3a231f16c3cb140665c9547c38681d05c227a
Sean Molenaar <smillerdev@me.com>
2021-03-03 11:07:33 +0100

    php-uopz: split for php 7.4
    Closes #66.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

--- Formula/php@7.4-uopz.rb
+++ Formula/php@7.4-uopz.rb
@@ -1,27 +1,25 @@
-class PhpUopz < Formula
+class PhpAT74Uopz < Formula
   desc "UOPZ Extension for PHP"
   homepage "https://pecl.php.net/uopz"
   url "https://pecl.php.net/get/uopz-6.1.2.tgz"
   sha256 "d84b8a2ed89afc174be2dd2771410029deaf5796a2473162f94681885a4d39a8"
   head "https://github.com/krakjoe/uopz.git"
 
-  disable! date: "2021-03-01", because: "support pending" unless build.head?
-
   depends_on "autoconf" => :build
   depends_on "pkg-config" => :build
-  depends_on "php"
+  depends_on "php@7.4"
 
   def module_path
-    extension_dir = Utils.safe_popen_read("#{Formula["php"].opt_bin/"php-config"} --extension-dir").chomp
+    extension_dir = Utils.safe_popen_read("#{Formula["php@7.4"].opt_bin/"php-config"} --extension-dir").chomp
     php_basename = File.basename(extension_dir)
     "php/#{php_basename}"
   end
 
   def install
     cd "uopz-#{version}" unless build.head?
-    system Formula["php"].bin/"phpize"
+    system Formula["php@7.4"].bin/"phpize"
     configure_args = %W[
-      --with-php-config=#{Formula["php"].opt_bin/"php-config"}
+      --with-php-config=#{Formula["php@7.4"].opt_bin/"php-config"}
     ]
     system "./configure", *configure_args
     system "make"
@@ -29,7 +27,7 @@ class PhpUopz < Formula
   end
 
   def post_install
-    ext_config_path = etc/"php"/Formula["php"].version.major_minor/"conf.d"/"ext-uopz.ini"
+    ext_config_path = etc/"php"/Formula["php@7.4"].version.major_minor/"conf.d"/"ext-uopz.ini"
     if ext_config_path.exist?
       inreplace ext_config_path,
         /extension=.*$/, "extension=\"#{opt_lib/module_path}/uopz.so\""
@@ -42,7 +40,7 @@ class PhpUopz < Formula
   end
 
   test do
-    assert_match "uopz", shell_output("#{Formula["php"].opt_bin}/php -m").downcase,
+    assert_match "uopz", shell_output("#{Formula["php@7.4"].opt_bin}/php -m").downcase,
       "failed to find extension in php -m output"
   end
 end
