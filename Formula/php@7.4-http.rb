34baed207907286e099987987afabd8738dba4bb
Sean Molenaar <smillerdev@me.com>
2021-03-03 10:59:02 +0100

    php@7.4-http: new formula

---
+++ Formula/php@7.4-http.rb
@@ -0,0 +1,65 @@
+class PhpAT74Http < Formula
+  desc "Pecl HTTP Extension for PHP"
+  homepage "https://github.com/m6w6/ext-http"
+  url "https://pecl.php.net/get/pecl_http-3.2.3.tgz"
+  sha256 "cf9d2a2ed335e572366025eca1d69e86c585f96ca07db341839a06f52ee4aa88"
+  head "https://github.com/m6w6/ext-http.git"
+
+  depends_on "autoconf" => :build
+  depends_on "pkg-config" => :build
+  depends_on "php@7.4"
+  depends_on "php@7.4-propro"
+  depends_on "php@7.4-raphf"
+
+  def module_path
+    extension_dir = Utils.safe_popen_read("#{Formula["php@7.4"].opt_bin/"php-config"} --extension-dir").chomp
+    php_basename = File.basename(extension_dir)
+    "php/#{php_basename}"
+  end
+
+  def install
+    cd "pecl_http-#{version}"
+    system Formula["php@7.4"].bin/"phpize"
+
+    mkdir "ext"
+    # link in the raphf extension header
+    cp_r "#{Formula["php@7.4-raphf"].include}/raphf@7.4", "ext/raphf"
+
+    # link in the propro extension header
+    cp_r "#{Formula["php@7.4-propro"].include}/propro@7.4", "ext/propro"
+
+    configure_args = %W[
+      --with-http
+      --without-http-libidn-dir
+      --without-http-libidn2-dir
+      --without-http-libidnkit-dir
+      --without-http-libidnkit2-dir
+      --with-libdir=/usr/local/lib
+      --with-php-config=#{Formula["php@7.4"].opt_bin/"php-config"}
+      --with-http-zlib-dir=#{MacOS.sdk_path_if_needed}/usr
+      --with-http-libcurl-dir=#{MacOS.sdk_path_if_needed}/usr
+      --with-http-libicu-dir=#{MacOS.sdk_path_if_needed}/usr
+    ]
+    system "./configure", *configure_args
+    system "make"
+    (lib/module_path).install "modules/http.so"
+  end
+
+  def post_install
+    ext_config_path = etc/"php"/Formula["php@7.4"].version.major_minor/"conf.d"/"ext-http.ini"
+    if ext_config_path.exist?
+      inreplace ext_config_path,
+        /extension=.*$/, "extension=\"#{opt_lib/module_path}/http.so\""
+    else
+      ext_config_path.write <<~EOS
+        [pecl_http]
+        extension=\"#{opt_lib/module_path}/http.so\"
+      EOS
+    end
+  end
+
+  test do
+    assert_match "http", shell_output("#{Formula["php@7.4"].opt_bin}/php -m").downcase,
+      "failed to find extension in php -m output"
+  end
+end
