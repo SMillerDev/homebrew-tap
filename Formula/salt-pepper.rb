affd3e1999314a271aa7fdd28078acf5aa7f52f3
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-03-04 20:00:44 +0000

    salt-pepper: add 0.7.6 bottle.

--- Formula/salt-pepper.rb
+++ Formula/salt-pepper.rb
@@ -7,6 +7,12 @@ class SaltPepper < Formula
   sha256 "b75a641d4fd96663ae44fc7ce4aadb7e4c87b8ff30f7ac35479a282b99894749"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/salt-pepper-0.7.6"
+    sha256 cellar: :any_skip_relocation, catalina:     "12cca8c6274bae7ab56042bdf2dc563209305b9dedd38f2eb8d6428ecffecaf0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "77895e4a224deebb17b64888c04d8baaa31452691fbebbe911ffc22886cfd374"
+  end
+
   depends_on "python@3.9"
 
   def install
