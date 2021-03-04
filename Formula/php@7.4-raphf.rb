9a9ca0fcf3b8e204f1f2e644b64f9089b0c951fc
Sean Molenaar <smillerdev@me.com>
2021-03-04 16:54:34 +0100

    php@7.4-raphf: split for php 7.4

---
+++ Formula/php@7.4-raphf.rb
@@ -0,0 +1,49 @@
+class PhpAT74Raphf < Formula
+  desc "Raphf Extension for PHP"
+  homepage "https://pecl.php.net/raphf"
+  url "https://pecl.php.net/get/raphf-2.0.1.tgz"
+  sha256 "da3566db17422e5ef08b7ff144162952aabc14cb22407cc6b1d2a2d095812bd0"
+  head "https://github.com/m6w6/ext-raphf.git"
+
+  depends_on "autoconf" => :build
+  depends_on "pkg-config" => :build
+  depends_on "php@7.4"
+
+  def module_path
+    extension_dir = Utils.safe_popen_read("#{Formula["php@7.4"].opt_bin/"php-config"} --extension-dir").chomp
+    php_basename = File.basename(extension_dir)
+    "php/#{php_basename}"
+  end
+
+  def install
+    cd "raphf-#{version}"
+    system Formula["php@7.4"].bin/"phpize"
+    configure_args = %W[
+      --with-php-config=#{Formula["php@7.4"].opt_bin/"php-config"}
+    ]
+    system "./configure", *configure_args
+    system "make"
+
+    mkdir_p include/"raphf@7.4"
+    (include/"raphf@7.4").install %w[php_raphf.h src/php_raphf_api.h]
+    (lib/module_path).install "modules/raphf.so"
+  end
+
+  def post_install
+    ext_config_path = etc/"php"/Formula["php@7.4"].version.major_minor/"conf.d"/"ext-raphf.ini"
+    if ext_config_path.exist?
+      inreplace ext_config_path,
+        /extension=.*$/, "extension=\"#{opt_lib/module_path}/raphf.so\""
+    else
+      ext_config_path.write <<~EOS
+        [raphf]
+        extension=\"#{opt_lib/module_path}/raphf.so\"
+      EOS
+    end
+  end
+
+  test do
+    assert_match "raphf", shell_output("#{Formula["php@7.4"].opt_bin}/php -m").downcase,
+      "failed to find extension in php -m output"
+  end
+end
