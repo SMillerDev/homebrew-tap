9bef77a0e0f7f5167fd3e192ca579ae69545d570
Sean Molenaar <smillerdev@me.com>
2021-03-04 20:35:36 +0100

    salt-pepper: 0.7.6
    Closes #71.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

--- Formula/salt-pepper.rb
+++ Formula/salt-pepper.rb
@@ -1,23 +1,22 @@
 class SaltPepper < Formula
   include Language::Python::Virtualenv
 
-  desc "Library and CLI scripts for accessing a salt-api instance"
-  homepage "https://saltstack.com"
-  url "https://files.pythonhosted.org/packages/1b/3f/9213b4bab514aadc48f0a89d5bc218d9e042366c16eb75b0c0c3d7fdb134/salt-pepper-0.7.5.tar.gz"
-  sha256 "9ca4daebbe15c4da0ca286d74f7361444cfd30fc6abc92afdc12cdc65bf406f2"
-  head "https://github.com/saltstack/pepper.git"
+  desc "Python library and CLI scripts for accessing a remote salt-api instance"
+  homepage "https://github.com/saltstack/pepper/"
+  url "https://files.pythonhosted.org/packages/5a/e0/2aa7893ccd711fbb197e1b96038c47db9d903604e023a436521780775d5b/salt-pepper-0.7.6.tar.gz"
+  sha256 "b75a641d4fd96663ae44fc7ce4aadb7e4c87b8ff30f7ac35479a282b99894749"
+  license "Apache-2.0"
 
-  depends_on "python"
+  depends_on "python@3.9"
 
   def install
-    virtualenv_create(libexec, "python")
     virtualenv_install_with_resources
   end
 
   test do
-    out = shell_output("#{bin}/pepper
-      --saltapi-url=https://localhost:8000
-      --username='test'
+    out = shell_output("#{bin}/pepper \
+      --saltapi-url=https://localhost:8000 \
+      --username='test' \
       --password='f' '*' test.ping 2>&1", 1)
     assert_match "Connection refused", out
   end
