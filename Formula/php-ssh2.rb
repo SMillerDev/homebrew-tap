9da621874651dde852ca842f9d59cd4310c38ba7
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-03-05 09:51:44 +0000

    php-ssh2: add 1.3.1 bottle.

--- Formula/php-ssh2.rb
+++ Formula/php-ssh2.rb
@@ -5,6 +5,12 @@ class PhpSsh2 < Formula
   sha256 "9093a1f8d24dc65836027b0e239c50de8d5eaebf8396bc3331fdd38c5d69afd9"
   head "https://git.php.net/repository/pecl/networking/ssh2.git"
 
+  bottle do
+    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/php-ssh2-1.3.1"
+    sha256                               catalina:     "f90bbe34026429a959813b4b4c5aae2744b2bd8fbda84bec5f8abc1b80897bbb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3189a4208def169a45f132e986ff9e147d045d2a7e28676492322d82250254e7"
+  end
+
   depends_on "autoconf" => :build
   depends_on "pkg-config" => :build
 
