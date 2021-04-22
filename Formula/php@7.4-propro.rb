802d2ea7fdb640d48a74ef36d3246a50053dc5d6
Sean Molenaar <smillerdev@me.com>
2021-04-22 16:06:24 +0200

    php@7.4-propro: add livecheck

--- Formula/php@7.4-propro.rb
+++ Formula/php@7.4-propro.rb
@@ -4,6 +4,10 @@ class PhpAT74Propro < Formula
   url "https://pecl.php.net/get/propro-2.1.0.tgz"
   sha256 "7bba0653d90cd8f61816e13ac6c0f7102b4a16dc4c4e966095a121eeb4ae8271"
   head "https://github.com/m6w6/ext-propro.git"
+  
+  livecheck do
+    url :stable
+  end
 
   bottle do
     root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/php@7.4-propro-2.1.0"
