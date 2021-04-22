32f0972a0fd8b7a4e17892149a059414afaffeb0
Sean Molenaar <smillerdev@me.com>
2021-04-22 16:05:12 +0200

    php@7.4-autoload-psr: add livecheck

--- Formula/php@7.4-autoload-psr.rb
+++ Formula/php@7.4-autoload-psr.rb
@@ -5,6 +5,10 @@ class PhpAT74AutoloadPsr < Formula
   sha256 "edee4c0fb8d66238509e080697a868ad454444242ffb269cb52c6cceabd04c2c"
   head "https://github.com/pprkut/autoload-psr.git"
 
+  livecheck do
+    url :stable
+  end
+
   bottle do
     root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/php@7.4-autoload-psr-0.1.2"
     sha256 cellar: :any_skip_relocation, catalina:     "8a523bd5672c1d2027f9fc34eaffecdceb629a1796bccfc3b3f6b1bcd605c0c7"
