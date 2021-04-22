123dfcb9f37b94a0dc9a6c00082647401d4b9a28
Sean Molenaar <smillerdev@me.com>
2021-04-22 16:12:01 +0200

    php-http 4.1.0
    Closes #82.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

--- Formula/php-http.rb
+++ Formula/php-http.rb
@@ -1,8 +1,8 @@
 class PhpHttp < Formula
   desc "Pecl HTTP Extension for PHP"
   homepage "https://github.com/m6w6/ext-http"
-  url "https://pecl.php.net/get/pecl_http-4.0.0.tgz"
-  sha256 "ea9a508578cffd428baf7b78f6d1618badedf3175be06b0809588a8b48889d5f"
+  url "https://pecl.php.net/get/pecl_http-4.1.0.tgz"
+  sha256 "f8b3a0827c4c8c97ef00d51d09c63fa4203f895bd737f954062c16bae3b8ab1e"
   head "https://github.com/m6w6/ext-http.git"
 
   bottle do
