d3f97cc066ead84a8472ddeed316fda030eb218b
Sean Molenaar <smillerdev@me.com>
2021-06-04 15:01:06 +0200

    jsonschema 4.0.0a1
    Closes #85.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

--- Formula/jsonschema.rb
+++ Formula/jsonschema.rb
@@ -3,10 +3,8 @@ class Jsonschema < Formula
 
   desc "Implementation of JSON Schema for Python"
   homepage "https://github.com/Julian/jsonschema"
-  url "https://github.com/Julian/jsonschema.git",
-    revision: "5e0fea13189bf118eadc8c812eed08f0751b8394"
-  version "3.2.99"
-  revision 1
+  url "https://files.pythonhosted.org/packages/ee/10/f2137346cd9568f1a92a9c6c12b086bc14223f16e90ab04d8c8edf36d39d/jsonschema-4.0.0a1.tar.gz"
+  sha256 "6fbbcc8035b0d8722146b8a90ada08fcd910b731aa0c920b757be9c081bc849f"
   head "https://github.com/Julian/jsonschema.git"
 
   bottle do
@@ -15,31 +13,17 @@ class Jsonschema < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "75d496f68f15c3c40b64fa767200820a54cb0caab2d688922b4af3a22e6bab97"
   end
 
-  depends_on "python@3.8"
+  depends_on "python@3.9"
+  depends_on "six"
 
   resource "attrs" do
-    url "https://files.pythonhosted.org/packages/98/c3/2c227e66b5e896e15ccdae2e00bbc69aa46e9a8ce8869cc5fa96310bf612/attrs-19.3.0.tar.gz"
-    sha256 "f7b7ce16570fe9965acd6d30101a28f62fb4a7f9e926b3bbc9b61f8b04247e72"
-  end
-
-  resource "more-itertools" do
-    url "https://files.pythonhosted.org/packages/df/8c/c278395367a46c00d28036143fdc6583db8f98622b83875403f16473509b/more-itertools-8.1.0.tar.gz"
-    sha256 "c468adec578380b6281a114cb8a5db34eb1116277da92d7c46f904f0b52d3288"
+    url "https://files.pythonhosted.org/packages/ed/d6/3ebca4ca65157c12bd08a63e20ac0bdc21ac7f3694040711f9fd073c0ffb/attrs-21.2.0.tar.gz"
+    sha256 "ef6aaac3ca6cd92904cdd0d83f629a15f18053ec84e6432106f7a4d04ae4f5fb"
   end
 
   resource "pyrsistent" do
-    url "https://files.pythonhosted.org/packages/90/aa/cdcf7ef88cc0f831b6f14c8c57318824c9de9913fe8de38e46a98c069a35/pyrsistent-0.15.7.tar.gz"
-    sha256 "cdc7b5e3ed77bed61270a47d35434a30617b9becdf2478af76ad2c6ade307280"
-  end
-
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/21/9f/b251f7f8a76dec1d6651be194dfba8fb8d7781d10ab3987190de8391d08e/six-1.14.0.tar.gz"
-    sha256 "236bdbdce46e6e6a3d61a337c0f8b763ca1e8717c03b369e87a7ec7ce1319c0a"
-  end
-
-  resource "zipp" do
-    url "https://files.pythonhosted.org/packages/d4/cd/ef86396dce8910413b6ca1ef31ec09367c47e15fc1a12def2cc8ae134dea/zipp-1.0.0.tar.gz"
-    sha256 "d38fbe01bbf7a3593a32bc35a9c4453c32bc42b98c377f9bff7e9f8da157786c"
+    url "https://files.pythonhosted.org/packages/4d/70/fd441df751ba8b620e03fd2d2d9ca902103119616f0f6cc42e6405035062/pyrsistent-0.17.3.tar.gz"
+    sha256 "2e636185d9eb976a18a8a8e96efce62f2905fea90041958d8cc2a189756ebf3e"
   end
 
   def install
