a5994ea7cdb041a062064b84d88b7a9c8b4091b6
Sean Molenaar <smillerdev@me.com>
2021-09-20 10:38:21 +0200

    php-raphf: add order to extensions

--- Formula/php-raphf.rb
+++ Formula/php-raphf.rb
@@ -37,7 +37,7 @@ class PhpRaphf < Formula
   end
 
   def post_install
-    ext_config_path = etc/"php"/Formula["php"].version.major_minor/"conf.d"/"ext-raphf.ini"
+    ext_config_path = etc/"php"/Formula["php"].version.major_minor/"conf.d"/"10-ext-raphf.ini"
     if ext_config_path.exist?
       inreplace ext_config_path,
         /extension=.*$/, "extension=\"#{opt_lib/module_path}/raphf.so\""
