ff7ebe90c9641a872ca18f7088ae7854eb0df6f3
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-10-23 17:06:37 +0000

    phing: add 2.17.0 bottle.

--- Formula/phing.rb
+++ Formula/phing.rb
@@ -5,6 +5,12 @@ class Phing < Formula
   sha256 "1703462b5c3ab7841c760cbae6a4a7f3cfda2acd715fd34b663ab13f2ac96546"
   head "https://github.com/phingofficial/phing.git"
 
+  bottle do
+    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/phing-2.17.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "4cf6f16d64a12ac6e19ca580a0b0687ebf4fe16e6045a3f3e7c5c5e13d99daf1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c134c51d43e0b70d040025d473d07d166249a199f73e66eeee3ff87c19a07689"
+  end
+
   depends_on "php" => :test
 
   def install
