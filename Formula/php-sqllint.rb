f617e90be074a31651946a353744092c75a96cf6
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2021-10-23 17:06:37 +0000

    php-sqllint: add 0.2.3 bottle.

--- Formula/php-sqllint.rb
+++ Formula/php-sqllint.rb
@@ -5,6 +5,12 @@ class PhpSqllint < Formula
   sha256 "a02b50ee2bfd20a67da9c6786b72611c9c1bdd969ed1fa1a3565644881b095df"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/php-sqllint-0.2.3"
+    sha256 cellar: :any_skip_relocation, catalina:     "5e758e3bca6f5c06680dd3ebdcb8edb083a5926ffb845998e9e597b1567e9c85"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "85812fa661667b6c5ebd2055b904ed20dcd7d5842faa6469fd72dba77bce06b7"
+  end
+
   depends_on "php"
 
   def install
