e0dcba610d29b262ce4dbf38f4aa62cf184553fb
Sean Molenaar <smillerdev@me.com>
2021-11-17 18:08:34 +0100

    teleport@7 7.3.3 (new formula)

---
+++ Formula/teleport@7.rb
@@ -0,0 +1,50 @@
+class TeleportAT7 < Formula
+  desc "Modern SSH server for teams managing distributed infrastructure"
+  homepage "https://gravitational.com/teleport"
+  url "https://github.com/gravitational/teleport/archive/v7.3.3.tar.gz"
+  sha256 "893fb8e06f28eb5ee48989bf8e4fa2358a41d65d3c0372fe95076c948b530514"
+  license "Apache-2.0"
+  head "https://github.com/gravitational/teleport.git", branch: "master"
+
+  # This version will live as long as I'm affected by https://github.com/gravitational/teleport/issues/9028
+  deprecate! date: "2021-11-17", because: :versioned_formula
+
+  depends_on "go" => :build
+
+  uses_from_macos "curl" => :test
+  uses_from_macos "netcat" => :test
+  uses_from_macos "zip"
+
+  conflicts_with "etsh", because: "both install `tsh` binaries"
+
+  # Keep this in sync with https://github.com/gravitational/teleport/tree/v#{version}
+  resource "webassets" do
+    url "https://github.com/gravitational/webassets/archive/07493a5e78677de448b0e35bd72bf1dc6498b5ea.tar.gz"
+    sha256 "2074ee7e50720f20ff1b4da923434c05f6e1664e13694adde9522bf9ab09e0fd"
+  end
+
+  def install
+    (buildpath/"webassets").install resource("webassets")
+    ENV.deparallelize { system "make", "full" }
+    bin.install Dir["build/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/teleport version")
+    (testpath/"config.yml").write shell_output("#{bin}/teleport configure")
+      .gsub("0.0.0.0", "127.0.0.1")
+      .gsub("/var/lib/teleport", testpath)
+      .gsub("/var/run", testpath)
+      .gsub(/https_(.*)/, "")
+
+    fork do
+      exec "#{bin}/teleport start -c #{testpath}/config.yml --debug"
+    end
+
+    sleep 10
+    system "curl", "--insecure", "https://localhost:3080"
+    system "nc", "-z", "localhost", "3022"
+    system "nc", "-z", "localhost", "3023"
+    system "nc", "-z", "localhost", "3025"
+  end
+end
