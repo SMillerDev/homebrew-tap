76dc92444dfc1506c50b518ceada0d3d554c0945
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2025-01-10 11:30:04 +0000

    php-http@8.3: add 4.2.6 bottle.

--- Formula/php-http@8.3.rb
+++ Formula/php-http@8.3.rb
@@ -5,6 +5,13 @@ class PhpHttpAT83 < Formula
   sha256 "cd33230050b3f7c5ddb6f4383ce2a81f0bcdb934432029eec72ebf0f942b876d"
   head "https://github.com/m6w6/ext-http.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/php-http@8.3-4.2.6"
+    sha256 cellar: :any,                 arm64_sonoma: "5a8f7786cd24077958a55643d21210c8cdce24d0d82b39e5a2e722754622a4fb"
+    sha256 cellar: :any,                 ventura:      "6811be2b8e9e1cd72a6cb263f1639c1b94a963a8b3432d989f163365b9fa40ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "32c48a7e80f6012f4e9aaf5b1796ff2ac253ddf6952c8572a320e638c00945dc"
+  end
+
   depends_on "autoconf" => :build
   depends_on "pkg-config" => :build
   depends_on "brotli"
