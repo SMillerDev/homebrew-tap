f5a3f14fb4717532e45a9ca4d4e23cac406c37f9
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2026-06-23 23:03:08 +0000

    php-raphf: update 2.0.1_5 bottle.

--- Formula/php-raphf.rb
+++ Formula/php-raphf.rb
@@ -8,9 +8,10 @@ class PhpRaphf < Formula
 
   bottle do
     root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/php-raphf-2.0.1_5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "8e68874fc3668fa0f03a60f2a6a185111314e5c4db54a0a1f2e5b9df0d0ae40e"
-    sha256 cellar: :any_skip_relocation, ventura:      "05514853bdbb3fda890762ed92952f13eb3fc5b5a4c0181d1233a1ce7bc85e78"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f4885e5b6345233f6c2dd4fd50707d7ed44a8abbd75bac35484fa767547a60b7"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "c50c0ce163debca8f6168881b8396f63d5dfea4abeb11134c16cf62b1aaf8554"
+    sha256 cellar: :any,                 arm64_linux:  "16659744e048b9fe086d09d64ccfa67bacc9404c5bb376f2808cd683d523d2d1"
+    sha256 cellar: :any,                 x86_64_linux: "48ca4bd7ea77bb7269a8b29443a4f0ef2956a67b988381fe7d28b928677b6f05"
   end
 
   depends_on "autoconf" => :build
