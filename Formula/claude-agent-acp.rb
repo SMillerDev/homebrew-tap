50d5c7b1e467dbd9878b70d5b22e1a6c35a479ff
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2026-06-24 07:03:43 +0000

    claude-agent-acp: add 0.49.0 bottle.

--- Formula/claude-agent-acp.rb
+++ Formula/claude-agent-acp.rb
@@ -5,6 +5,13 @@ class ClaudeAgentAcp < Formula
   sha256 "28165efacdc83dfdd5bf1d993e0a8b6ecb9fb52aee65ba8abdd623b2253685d3"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/claude-agent-acp-0.49.0"
+    sha256                               arm64_tahoe:  "95fb5c25b4d7c9fdaa79139a01047775b13199443633cc37d10ea71d30015a82"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "d7ea2aa254062100384389a1a9ac4a6f354b1dca164844f7004972822d082a57"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6e9ad6a2c40d206f5cdba7cba1f8afe7066f8457fc9985ff7f1f3748489937f6"
+  end
+
   depends_on "node"
 
   def install
