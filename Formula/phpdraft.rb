b73b35663fc74c724c106621cd493662be835221
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2026-07-06 09:49:30 +0000

    phpdraft 2.6.1

--- Formula/phpdraft.rb
+++ Formula/phpdraft.rb
@@ -1,9 +1,9 @@
 class Phpdraft < Formula
   desc "Parser for API Blueprint files in PHP"
   homepage "https://github.com/SMillerDev/phpdraft"
-  url "https://github.com/SMillerDev/phpdraft/releases/download/2.6.0/phpdraft-2.6.0.phar"
-  sha256 "ac6849af7faa3c5c7a0c0601f60bc999afe5407a74c47dfa9c56dd709432879e"
-  head "https://github.com/SMillerDev/phpdraft.git"
+  url "https://github.com/SMillerDev/phpdraft/releases/download/2.6.1/phpdraft-2.6.1.phar"
+  sha256 "3cdce47bb45c8cabee5f4a35b3bc47ea93abc81549e3e54b5c327cf3b631c5be"
+  head "https://github.com/SMillerDev/phpdraft.git", branch: "main"
 
   bottle do
     root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/phpdraft-2.6.0"
@@ -11,6 +11,8 @@ class Phpdraft < Formula
     sha256 cellar: :any_skip_relocation, ventura:      "5e09537c0acb9f5df8b934c0d81924a11b913d5122a3ccf4aad6e3b94f979b47"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "4606c14af9847e6b190ca65739f48eca98153e5af418c927c7eb77325db639fd"
   end
+
+  deprecate! date: "2026-07-01", because: :repo_archived
 
   depends_on "php" => :test
   depends_on "drafter"
