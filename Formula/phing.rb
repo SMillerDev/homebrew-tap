0d2b90fa18877e01c06138a0a9f7a068dcb2d9bc
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2026-07-06 10:10:34 +0000

    phing: update 3.1.2 bottle.

--- Formula/phing.rb
+++ Formula/phing.rb
@@ -6,10 +6,11 @@ class Phing < Formula
   head "https://github.com/phingofficial/phing.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/phing-3.0.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb837412c6a46e33423c8b3461e80ee795964be8da824913c74cf6507361c663"
-    sha256 cellar: :any_skip_relocation, ventura:       "835711608e2618ab5c3a281d91415e9bc446e7568d5c16b3e77deedb0cb387db"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "56734f63555dee408cfbea4b1399ea69b7440acb8a5d9b7b885855c83375eecc"
+    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/phing-3.1.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "2bba0ef772b12b96cd545a8dbc3906d0fca253859184f6fc8b5cd8ac4000661c"
+    sha256 cellar: :any_skip_relocation, tahoe:        "92d357259c965a0a8117807ca66a88a0237d5c5d4ba510918003c37cd10ea522"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "a16c1dfbc97dc23a4b0603ec5e5ecb326044323c1233b8bbcaac796ae24ac91b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3775c56e574b45e58f82e6f4f06cfd5c51d8a5aa288025e915662abc09a29aec"
   end
 
   depends_on "php" => :test
