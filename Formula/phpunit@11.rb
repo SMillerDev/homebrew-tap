157cf719dca480dcc3ac343e8f8a3c6ddc2b7b11
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2026-07-08 09:42:50 +0000

    phpunit@11: update 11.5.56 bottle.

--- Formula/phpunit@11.rb
+++ Formula/phpunit@11.rb
@@ -11,11 +11,11 @@ class PhpunitAT11 < Formula
   end
 
   bottle do
-    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/phpunit@11-11.5.55"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "8cdb409486aa3643d4a13e40d6c65322506b0a075f021fa5d6d0cedc730e5886"
-    sha256 cellar: :any_skip_relocation, tahoe:        "ecbe430ac7e00e8f5f737b573db9fd88efd023af75cbe94c45e58df05ced12c1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "9218a26ccadcf46919e85752c65ed251285875fb7e79d2e3fe76b62f9702a637"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "812a10699f0fe73bece1f2921ccf6ec4fa20c60e4723648eb0f12382492689aa"
+    root_url "https://github.com/SMillerDev/homebrew-tap/releases/download/phpunit@11-11.5.56"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "837955f46234940cf92180311b426028c204fc4724a6de1f96c9a3112a3181d4"
+    sha256 cellar: :any_skip_relocation, tahoe:        "725c5597f20e33fe67bb3a0c72fe8f58e15b5ef876867a3584e65005bc6c3b01"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "bf8f2643b78ee0fc5d3d8a00e32403b42039fafcff532e7bd3334964dd33c2bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4330cb9fe6a21a24b5782bb3f213c46ab4916adc2d453791c58931d50740b73a"
   end
 
   depends_on "php" => :test
