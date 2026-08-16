7db0bd06730f251d10f12e64a54b50d141b8c9de
Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>
2026-08-16 02:47:59 +0000

    claude-agent-acp 0.68.0
    Closes #203.
    
    Signed-off-by: Sean Molenaar <1484494+SMillerDev@users.noreply.github.com>

--- Formula/claude-agent-acp.rb
+++ Formula/claude-agent-acp.rb
@@ -1,8 +1,8 @@
 class ClaudeAgentAcp < Formula
   desc "Use Claude Agent SDK from ACP-compatible clients"
   homepage "https://github.com/agentclientprotocol/claude-agent-acp"
-  url "https://registry.npmjs.org/@agentclientprotocol/claude-agent-acp/-/claude-agent-acp-0.66.0.tgz"
-  sha256 "01d63f87e75a7417edd1b7364fb471da1cf4650af6098ab257bb3fbd5dbae558"
+  url "https://registry.npmjs.org/@agentclientprotocol/claude-agent-acp/-/claude-agent-acp-0.68.0.tgz"
+  sha256 "6fb5039bcbcd0422a80809432871fc837d6164be1a3386dece323cfc39171afa"
   license "Apache-2.0"
 
   bottle do
