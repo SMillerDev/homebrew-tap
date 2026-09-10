8a18bc4a1ef9110abb80be59b04f90d4bd28e2c4
Sean Molenaar <sean@seanmolenaar.eu>
2026-09-10 18:00:42 +0300

    php-autoload-psr@8.3: use postinstall steps

--- Formula/php-autoload-psr@8.3.rb
+++ Formula/php-autoload-psr@8.3.rb
@@ -27,18 +27,16 @@ class PhpAutoloadPsrAT83 < Formula
     system "./configure", "--with-php-config=#{formula_opt_bin("php@8.3")/"php-config"}"
     system "make"
     (lib/module_path).install "modules/autoload_psr.so"
+    (pkgetc/"ext-autoload-psr.ini").write <<~EOS
+      [autoload_psr]
+      extension="#{opt_lib/module_path}/autoload_psr.so"
+    EOS
   end
 
-  def post_install
-    ext_config_path = etc/"php/#{Formula["php@8.3"].version.major_minor}/conf.d/ext-autoload_psr.ini"
-    if ext_config_path.exist?
-      inreplace ext_config_path,
-        /extension=.*$/, "extension=\"#{opt_lib/module_path}/autoload_psr.so\""
-    else
-      ext_config_path.write <<~EOS
-        [autoload_psr]
-        extension="#{opt_lib/module_path}/autoload_psr.so"
-      EOS
+  post_install_steps do
+    unless_path_exists "php/8.3/conf.d/ext-autoload-psr.ini", base: :etc do
+      copy "ext-autoload-psr.ini", "php/8.3/conf.d/ext-autoload-psr.ini",
+           source_base: :pkgetc, target_base: :etc
     end
   end
 
